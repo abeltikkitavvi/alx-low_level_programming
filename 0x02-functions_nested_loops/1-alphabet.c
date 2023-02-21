d86e034a3bd1b32c2b0be516d792e7d735b652b7
abeltikkitavvi <abelomenge064@gmail.com>
2023-02-21 14:31:34 -0800

    fixed betty error in the program

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- *  prints_alphabet -  prints the alphabet in lowercase followed by a new line
+ * print_alphabet -  prints the alphabet in lowercase followed by a new line
+ * Return: void
  */
 void print_alphabet(void)
 {
