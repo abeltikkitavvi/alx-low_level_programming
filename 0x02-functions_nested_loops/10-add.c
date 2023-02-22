5c0bf276f1c84ed6f4ae03c5809c698a55018604
abeltikkitavvi <abelomenge064@gmail.com>
2023-02-22 08:19:46 -0800

    pragram that ads tow numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @d: the first number
+ * @e: the first number
+ * Return: result of adding both
+ */
+int add(int d, int e)
+{
+	return (d + e);
+}
