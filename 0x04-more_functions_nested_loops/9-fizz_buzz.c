7243bd8214023790eca22ef0bd008254aecb35d3
abeltikkitavvi <abelomenge064@gmail.com>
2023-02-24 18:26:12 -0800

    final changes to fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,20 +6,28 @@
  */
 int main(void)
 {
+
 	int k;
 
 	for (k = 1; k <= 100; k++)
 	{
-		printf(" ");
-		if (k % 3 == 0)
-			printf("Fizz");
-		if (k % 5 == 0)
-			printf("Buzz");
 		if (k % 3 == 0 && k % 5 == 0)
-			printf("FizzBuzz");
-		if (k % 3 != 0 && k % 5 != 0)
-			printf("%d", k);
+		{
+			printf("FizzBuzz ");
+		}
+		else if (k % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (k % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", k);
+		}
 	}
-	printf("\n");
 	return (0);
 }
+
