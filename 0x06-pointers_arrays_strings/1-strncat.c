4c8e6aeed60189ddee666ae16acafd4b7358cef7
abeltikkitavvi <abelomenge064@gmail.com>
2023-03-01 13:25:40 -0800

    project for concatenating task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *_strncat - a function that concatenates two strings
+ * @dest: copying into this
+ * @src: copying from this one
+ * @n: number of character to be copied
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	b = 0;
+	while (b < n && src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
