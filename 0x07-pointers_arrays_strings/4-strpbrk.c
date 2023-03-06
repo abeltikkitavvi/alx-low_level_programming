9a4f668419a18e1894e1b62550a387ccd56bd799
abeltikkitavvi <abelomenge064@gmail.com>
2023-03-06 13:34:29 -0800

    task 4 program that searches a string for any of a set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strpbrk - function that searches a string for any of a set of bytes
+ * @s: input
+ * @accept: input
+ * Return: always 0
+ */
+char *_strpbrk(char *s, char *accept)
+{
+		int c;
+
+		while (*s)
+		{
+			for (c = 0; accept[c]; c++)
+			{
+			if (*s == accept[c])
+			return (0);
+			}
+		s++;
+		}
+	return ('\0');
+}
+
